ec453c7d563575138e293463ff0ef3464eb26bc7
root <root@localhost.localdomain>
2015-10-19 08:46:05 -0400

    kali2.0_init.sh

---
+++ kali2.0_init.sh
@@ -0,0 +1,25 @@
+echo "set nu">>/root/.exrc
+echo "set nocompatible">>/root/.exrc
+echo "syntax on">>/root/.exrc
+echo "set autoindent">>/root/.exrc
+echo "set fileencodings=utf-8,gbk">>/root/.exrc
+
+echo "alias c='clear'">>/root/.bashrc
+echo "alias vi='vim'">>/root/.bashrc
+echo "alias ll='ls -l'">>/root/.bashrc
+echo "PATH=$PATH:.">>/root/.profile
+
+echo "deb http://mirrors.aliyun.com/kali kali main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://mirrors.aliyun.com/kali kali main non-free contrib">>/etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/kali-security kali/updates main contrib non-free">>/etc/apt/sources.list
+
+
+echo "deb http://mirrors.ustc.edu.cn/kali kali main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://mirrors.ustc.edu.cn/kali kali main non-free contrib">>/etc/apt/sources.list
+echo "deb http://mirrors.ustc.edu.cn/kali-security kali/updates main non-free contrib">>/etc/apt/sources.list
+
+echo "deb http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
+echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/sources.list
+echo "deb-src http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/sources.list
+
