5478329f068f9a4eff5c07eee8005318b41b6440
root <root@localhost.localdomain>
2015-10-20 16:03:34 -0400

    kali2.0_init.sh

--- kali2.0_init.sh
+++ kali2.0_init.sh
@@ -9,17 +9,31 @@ echo "alias vi='vim'">>/root/.bashrc
 echo "alias ll='ls -l'">>/root/.bashrc
 echo "PATH=$PATH:.">>/root/.profile
 
+echo "deb http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
+echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/source.list
+echo "deb-src http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/sources.list
+
 echo "deb http://mirrors.aliyun.com/kali kali main non-free contrib">>/etc/apt/sources.list
 echo "deb-src http://mirrors.aliyun.com/kali kali main non-free contrib">>/etc/apt/sources.list
 echo "deb http://mirrors.aliyun.com/kali-security kali/updates main contrib non-free">>/etc/apt/sources.list
 
+#echo "deb http://mirrors.ustc.edu.cn/kali kali main non-free contrib
+#echo "deb-src http://mirrors.ustc.edu.cn/kali kali main non-free contrib
+#echo "deb http://mirrors.ustc.edu.cn/kali-security kali/updates main non-free contrib
 
-echo "deb http://mirrors.ustc.edu.cn/kali kali main non-free contrib">>/etc/apt/sources.list
-echo "deb-src http://mirrors.ustc.edu.cn/kali kali main non-free contrib">>/etc/apt/sources.list
-echo "deb http://mirrors.ustc.edu.cn/kali-security kali/updates main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://ftp.sjtu.edu.cn/debian-security wheezy/updates main non-free contrib">>/etc/apt/sources.list
+echo "deb http://mirrors.163.com/debian wheezy main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://mirrors.163.com/debian wheezy main non-free contrib">>/etc/apt/sources.list
+echo "deb http://mirrors.163.com/debian wheezy-proposed-updates main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://mirrors.163.com/debian wheezy-proposed-updates main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://mirrors.163.com/debian-security wheezy/updates main non-free contrib">>/etc/apt/sources.list
+echo "deb http://mirrors.163.com/debian-security wheezy/updates main non-free contrib">>/etc/apt/sources.list
 
-echo "deb http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
-echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/sources.list
-echo "deb-src http://http.kali.org/kali sana main non-free contrib">>/etc/apt/sources.list
-echo "deb-src http://security.kali.org/kali-security sana/updates main contrib non-free">>/etc/apt/sources.list
+echo "deb http://ftp.sjtu.edu.cn/debian wheezy main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://ftp.sjtu.edu.cn/debian wheezy main non-free contrib">>/etc/apt/sources.list
+echo "deb http://ftp.sjtu.edu.cn/debian wheezy-proposed-updates main non-free contrib">>/etc/apt/sources.list
+echo "deb-src http://ftp.sjtu.edu.cn/debian wheezy-proposed-updates main non-free contrib">>/etc/apt/sources.list
+echo "deb http://ftp.sjtu.edu.cn/debian-security wheezy/updates main non-free contrib">>/etc/apt/sources.list
+
 
